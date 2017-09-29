bd848317df35fa71668eeb2fe554d111dbda8f08
Flo <x.client.ich@gmail.com>
2017-09-29 22:35:02 +0200

    redirect

--- server.js
+++ server.js
@@ -10,10 +10,35 @@ app.use(favicon(path.join(__dirname, 'public', 'imgs', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-app.locals.menu = [ { title: "Top Design", route: "topdesign" }, { title: "Home", route: "/" },{ title: "Levels", route: "levels"}];
-
+const redirects = {
+    discord: {
+        url: "https://discordapp.com/invite/PGv5TR3"
+    },
+    music: {
+        url: "http://46.228.192.55:8087"
+    },
+    rc: {
+        url: "https://www.dropbox.com/sh/ecq8lku300b63ir/AABxOnsQoEg1SP26klmHEBsva?dl=0"
+    },
+    signaltransmitter: {
+        url: "https://signaltransmitter.de/ref/234ra69r31584c44"
+    },
+    yt: {
+        url: "https://www.youtube.com/channel/UC5xEZufXUwoxdixe0QVDrtA"
+    }
+}
+app.locals.menu = [{
+    title: "Top Design",
+    route: "topdesign"
+}, {
+    title: "Home",
+    route: "/"
+}, {
+    title: "Levels",
+    route: "levels"
+}];
 let page = {};
+
 app.get('/', function (req, res) {
     res.render('index', page);
 });
@@ -26,6 +51,16 @@ app.get('/levels', function (req, res) {
 app.get('/donate', function (req, res) {
     res.render('donate', page);
 });
+app.get('/:keyword', function (req, res) {
+    const keyword = req.params.keyword
+    if (redirects[keyword]) {
+        res.redirect(redirects[keyword].url);
+    } else {
+        res.redirect('./')
+    }
+});
+
+
 
 const port = process.env.PORT || 7000;
 app.listen(port);

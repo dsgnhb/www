e96abbd62f48a0ff9291bdff7d83af686dfd4940
Fin Reinhard <fin.reinhard@icloud.com>
2017-10-02 22:36:46 +0200

    added Donations
    added PopUps

--- src/components/views/Donations.jsx
+++ src/components/views/Donations.jsx
@@ -1,24 +1,84 @@
 import React from 'react';
 import Text from '../general/misc/Text';
 import DiscordLink from '../general/misc/DiscordLink';
+import PopUp from '../general/misc/PopUp';
 import '../../files/css/twemoji.css';
+import paypal from '../../files/img/PayPal.png';
+import psc from '../../files/img/CardPayment.png';
 
-export default function Donations() {
-    return (
-        <div id="intro">
-            <div className="content">
-                <Text>
-                    Hiermit finanzieren wir <strong>Domains</strong>, ein bisschen <strong>Web-Hosting</strong> und
-                    alles andere, was halt sonst noch so Geld kostet. - <em>Und kostenlose Mega-Op Sachen für die Admins
-                    natürlich.</em>
-                    <i className="twa twa-lg twa-smirk" />
-                </Text>
-                <Text>
-                    Falls du uns unterstützen und das Projekt aufrecht erhalten möchtest, freuen wir uns über jede
-                    Hilfe!
-                </Text>
-                <DiscordLink />
+export default class Donations extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.openPSC = this.openPSC.bind(this);
+
+    }
+
+    openPSC() {
+        this.child.openNav();
+    }
+
+    render() {
+        return (
+            <div>
+                <PopUp
+                    ref={instance => {
+                        this.child = instance
+                    }}
+                    id="psc"
+                    title="Vielen Dank für deine Unterstützung mit PaysafeCard!"
+                >
+                    Da wir für Deine Spenden lieber auf ein komplexes Zahlungssystem mit hohen Gebühren/Abgaben
+                    verzichten, kontaktiere einfach
+                    <b>Flo (flo#3087)</b>
+                    auf unserem Discord per Direktnachricht.
+                </PopUp>
+                <div id="intro">
+                    <div className="content">
+                        <Text>
+                            Hiermit finanzieren wir <strong>Domains</strong>, ein bisschen <strong>Web-Hosting</strong>
+                            und
+                            alles andere, was halt sonst noch so Geld kostet. - <em>Und kostenlose Mega-Op Sachen für
+                            die
+                            Admins
+                            natürlich.</em>
+                            <i className="twa twa-lg twa-smirk" />
+                        </Text>
+                        <Text>
+                            Falls du uns unterstützen und das Projekt aufrecht erhalten möchtest, freuen wir uns über
+                            jede
+                            Hilfe!
+                        </Text>
+                        <DiscordLink />
+                    </div>
+                </div>
+                <div id="donate">
+                    <div className="content">
+                        <div className="methods">
+                            <div>
+                                <a href="https://www.paypal.me/juttawalter/1" target="_blank">
+                                    <img className="icon" src={paypal} />
+                                    <br />
+                                    <span>PayPal</span>
+                                </a>
+                            </div>
+                            <div>
+                                <a className="pointer" onClick={this.openPSC} href="#">
+                                    <img className="icon" src={psc} />
+                                    <br />
+                                    <span>PaySafeCard</span>
+                                </a>
+                            </div>
+                        </div>
+                        <Text title="Letzte Donations">
+                            Vielen Dank an:<br />
+                            - <a href="https://www.gommehd.net">@Teax</a><br />
+                            - <a href="https://twitter.com/pensivecauselov">@Bukki</a>
+                        </Text>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }

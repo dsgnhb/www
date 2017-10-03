4820df4c66b1109015a25b4445da216399054390
Fin Reinhard <fin.reinhard@icloud.com>
2017-10-03 14:10:51 +0200

    fixed Logo Link

--- src/components/general/Header.jsx
+++ src/components/general/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Menu from './Menu';
+import {Link} from 'react-router-dom';
 import logo from '../../files/img/logo.png';
 
 export default function Header() {
     return (
         <header>
             <div className="content">
-                <a href="/">
+                <Link to="/">
                     <img className="logo" src={logo} alt="Logo" />
-                </a>
+                </Link>
                 <h1>designhub.</h1>
                 <Menu />
             </div>

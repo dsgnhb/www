8a39df751669820c084964a65cbd18583dcb15f9
CreepPlays <CreepPlaysDE@users.noreply.github.com>
2017-10-30 16:14:26 +0100

    Fixed some missing spaces in topdesign view.

--- src/components/views/TopDesign.jsx
+++ src/components/views/TopDesign.jsx
@@ -13,14 +13,14 @@ export default function TopDesign() {
             erhält den @dsgnhb-Rang auf dem Discord-Server.
           </Text>
           <Text title="Teilnahme">
-            Füge zu deinem, auf unserem Discord geteilten Design einfach den Kommentar
-            <strong>#topdesign</strong>
+            Füge zu deinem, auf unserem Discord geteilten Design einfach den Kommentar 
+            <strong>#topdesign</strong> 
             hinzu, um an Top Design teilzunehmen. So wird deine Einsendung automatisch auf unserer Website angezeigt und kann von jedem angesehen und bewertet werden.
           </Text>
           <Text title="Voten/Liken">
             Um für ein Design zu voten, schreibe lediglich <strong>!vote #id</strong> in den Discord-Chat. Ersetze hierbei #id durch die Nummer der Einsendung, die du auf unserer Website durch einen
-            Klick auf das Design herausfinden kannst.<br /> Zusätzlich stehen dir noch die Commands
-            <strong>!topdesign</strong>, <strong>!posts</strong>, <strong>!delete</strong> sowie
+            Klick auf das Design herausfinden kannst.<br /> Zusätzlich stehen dir noch die Commands 
+            <strong>!topdesign</strong>, <strong>!posts</strong>, <strong>!delete</strong> sowie 
             <strong>!help</strong> zur Verfügung.
           </Text>
           <DiscordLink />

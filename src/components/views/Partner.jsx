b5044a475f88c1713507ae7752e5aebd38976f25
Flo <x.client.ich@gmail.com>
2017-11-01 22:05:43 +0100

    mega op texte

--- src/components/views/Partner.jsx
+++ src/components/views/Partner.jsx
@@ -3,51 +3,40 @@ import {Link} from 'react-router-dom';
 import {MainTitle, SectionTitle} from '../general/misc/Titles';
 
 export default function Partner() {
-    return (
-        <div>
-            <section id="partner">
-                <div className="container">
-                    <MainTitle
-                        title="Danke."
-                        subtitle="Partner"
-                        link="/discord"
-                        linktitle="Falls Du uns auch unterstützen möchtest, kannst Du das hier tun!"
-                    >
-                        Dank unserer Partner, die uns stets unterstützen, können wir das Projekt aktiv halten. Hier
-                        findest du alle Informationen rund um sämtliche aktive Partnerschaften. Solltest du das
-                        Interesse haben, ebenfalls eine Zusammenarbeit mit uns in Erwägung zu ziehen, schreib' uns ne
-                        Mail unter{' '}
-                        <Link to="/mail" className="main">
-                            info@dsgnhb.de
-                        </Link>.
-                    </MainTitle>
-                </div>
-            </section>
-            <PartnerInfo name="Signaltransmitter" task="Hosting" link="/signaltransmitter">
-                Alle Bots auf unserem Discord werden von den Jungs von Signaltransmitter gehostet. Du suchst einen guten
-                Hoster für deine Projekte und Ideen? Da empfehlen wir ganz klar Signaltransmitter!
-            </PartnerInfo>
-            <PartnerInfo name="ShortTech" task="Redaktionelle Inhalte" link="/shorttech">
-                Rund um Redaktionelle Inhalte ist das Team von ShortTech stets unser Ansprechpartner. In ihrem Blog
-                sowie auf Twitter erfährst du alles rund um die Welt der Technik!
-            </PartnerInfo>
-            <PartnerInfo name="smashFM" task="Musik" link="/smashfm">
-                Smash! Egal wann, egal wo! Dort hörst du nicht nur die aktuellsten Lieder, sondern auch Klassiker aus
-                vergangenen Zeiten. Von Charts über Dance bis hin zu Chillout ist bei denen alles dabei.
-            </PartnerInfo>
+  return (
+    <div>
+      <section id="partner">
+        <div className="container">
+          <MainTitle title="Danke." subtitle="Partner" link="/donate" linktitle="Falls Du uns auch unterstützen möchtest, kannst Du das hier tun!">
+            Dank unserer Partner, die uns stets unterstützen, können wir das Projekt aktiv halten. Hier findest du alle Informationen rund um sämtliche aktuelle Partnerschaften. Solltest du das
+            Interesse haben, ebenfalls eine Zusammenarbeit mit uns in Erwägung zu ziehen, schreib' uns ne Mail unter{' '}
+            <Link to="/mail" className="main">
+              info@dsgnhb.de
+            </Link>.
+          </MainTitle>
         </div>
-    );
+      </section>
+      <PartnerInfo name="Signaltransmitter" task="Hosting" link="/signaltransmitter">
+        Alle Bots auf unserem Discord werden von den Jungs von Signaltransmitter gehostet. Du suchst einen guten Hoster für deine Projekte und Ideen? Da empfehlen wir ganz klar Signaltransmitter!
+      </PartnerInfo>
+      <PartnerInfo name="ShortTech" task="Redaktionelle Inhalte" link="/shorttech">
+        Rund um Redaktionelle Inhalte ist das Team von ShortTech stets unser Ansprechpartner. In ihrem Blog sowie auf Twitter erfährst du alles rund um die Welt der Technik!
+      </PartnerInfo>
+      <PartnerInfo name="smashFM" task="Musik" link="/smashfm">
+        Smash! Egal wann, egal wo! Dort hörst du nicht nur die aktuellsten Lieder, sondern auch Klassiker aus vergangenen Zeiten. Von Charts über Dance bis hin zu Chillout ist bei denen alles dabei.
+      </PartnerInfo>
+    </div>
+  );
 }
 
 function PartnerInfo(props) {
-    return (
-        <section id={props.name.toLowerCase()}>
-            <div className="container">
-                <SectionTitle subtitle={props.task} title={props.name} link={props.link}
-                              linktitle="Hier geht's zur Website">
-                    {props.children}
-                </SectionTitle>
-            </div>
-        </section>
-    );
+  return (
+    <section id={props.name.toLowerCase()}>
+      <div className="container">
+        <SectionTitle subtitle={props.task} title={props.name} link={props.link} linktitle="Hier geht's zur Website">
+          {props.children}
+        </SectionTitle>
+      </div>
+    </section>
+  );
 }
--- src/components/views/branding/sections/Colors.jsx
+++ src/components/views/branding/sections/Colors.jsx
@@ -2,42 +2,41 @@ import React from 'react';
 import {SectionTitle} from '../../../general/misc/Titles';
 
 export default function Colors() {
-    return (
-        <section id="colors">
-            <div className="container">
-                <SectionTitle subtitle="Farben" title="Weiß-Grau">
-                    Der Content sollte immer im Kontrast zum Hintergrund stehen. Durch die richtige Nutzung unserer
-                    Farben können wir eine hohe Lesbarkeitbei allen Online- und Printanwendungen gewährleisten.
-                </SectionTitle>
-                <div className="content">
-                    <div className="flex-list icons">
-                        <Color id="one" hex="#ffffff" name="Wei&szlig;">
-                            Für Titel auf dunklem Hintergrund.
-                        </Color>
-                        <Color id="two" hex="#eeeeee" name="Hellgrau">
-                            Für Texte auf dunklem Hintergrund.
-                        </Color>
-                        <Color id="three" hex="#444444" name="Dunkelgrau">
-                            Für Texte auf hellem Hintergrund.
-                        </Color>
-                        <Color id="four" hex="#222222" name="Dunkelgrau">
-                            Für Titel auf hellem Hintergrund.
-                        </Color>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="colors">
+      <div className="container">
+        <SectionTitle subtitle="Farben" title="Weiß-Grau">
+          Der Content sollte immer im Kontrast zum Hintergrund stehen. Durch die richtige Nutzung unserer Farben können wir eine hohe Lesbarkeit bei allen Online- und Printanwendungen gewährleisten.
+        </SectionTitle>
+        <div className="content">
+          <div className="flex-list icons">
+            <Color id="one" hex="#ffffff" name="Wei&szlig;">
+              Für Titel auf dunklem Hintergrund.
+            </Color>
+            <Color id="two" hex="#eeeeee" name="Hellgrau">
+              Für Texte auf dunklem Hintergrund.
+            </Color>
+            <Color id="three" hex="#444444" name="Dunkelgrau">
+              Für Texte auf hellem Hintergrund.
+            </Color>
+            <Color id="four" hex="#222222" name="Mega-Dunkelgrau">
+              Für Titel auf hellem Hintergrund.
+            </Color>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 function Color(props) {
-    return (
-        <div className="item">
-            <div className={`example ${props.id} flex-center-center`}>
-                <h4>{props.hex}</h4>
-            </div>
-            <h3>{props.name}</h3>
-            <p>{props.children}</p>
-        </div>
-    );
+  return (
+    <div className="item">
+      <div className={`example ${props.id} flex-center-center`}>
+        <h4>{props.hex}</h4>
+      </div>
+      <h3>{props.name}</h3>
+      <p>{props.children}</p>
+    </div>
+  );
 }

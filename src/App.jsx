cc4483333fbd5475abcb1273798aefc9f76f86b4
Fin Reinhard <fin.reinhard@icloud.com>
2017-12-05 10:43:18 +0100

    added Piwik Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {Router} from 'react-router-dom';
+import PiwikReactRouter from 'piwik-react-router';
+import {createBrowserHistory} from 'history';
 import Typekit from 'react-typekit';
 import Header from './components/general/Header';
 import Routes from './components/general/Routes';
 import Footer from './components/general/Footer';
 import './files/css/App.css';
 
-function App() {
-    return (
-        <Router>
-            <div>
-                <Typekit kitId="vtp0hqt" />
-                <Header />
-                <Routes />
-                <Footer />
-            </div>
-        </Router>
-    );
-}
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default App;
+        this.history = createBrowserHistory();
+
+        this.piwik = PiwikReactRouter({
+            url: 'analytics.florentinwalter.de/',
+            siteId: 1
+        });
+    }
+
+    render() {
+        return (
+            <Router history={this.piwik.connectToHistory(this.history)}>
+                <div>
+                    <Typekit kitId="vtp0hqt" />
+                    <Header />
+                    <Routes />
+                    <Footer />
+                </div>
+            </Router>
+        );
+    }
+}

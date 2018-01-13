d08193a633f3a958878d4e3ea99a8162f8e93144
Flo <x.client.ich@gmail.com>
2018-01-13 21:39:03 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ls
 lftp -u $FTP_USER,$FTP_PASS $FTP_HOST -e "mirror -e -R  -p ./build/ / ; quit"
-sshpass -p '$SSH_PASS' scp -r ./build/ $SSH_USER@$SSH_HOST:/var/www/github
+sshpass -p '$SSH_PASS' scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -r ./build $SSH_USER@$SSH_HOST:/var/www/github

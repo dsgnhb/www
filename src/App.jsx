4d9f554f7e2087002ac1dc63b464905435e98d66
Flo <x.client.ich@gmail.com>
2018-01-14 15:03:56 +0100

    Crappy Nightmode

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import Routes from './components/general/Routes';
 import Footer from './components/general/Footer';
 import './files/css/App.css';
 
+// pls make this better
+var currentTime = new Date().getHours();
+if (currentTime > 22 && currentTime < 6) {
+    require('./files/css/night.css');
+}
+
 export default class App extends React.Component {
     constructor(props) {
         super(props);

0a250125f731c7b3cba2a95106739e5537934de5
Flo <x.client.ich@gmail.com>
2018-02-15 23:20:07 +0100

    disabled buttons + first icon for donations

--- src/components/views/donations/sections/Features.jsx
+++ src/components/views/donations/sections/Features.jsx
@@ -4,30 +4,30 @@ export default function Features() {
     return (
         <section id="donations-features">
             <div className="container flex-list icons left">
-                <Category id="vip" name="Zugang zum VIP Channel">
+                <Feature id="vip" name="Zugang zum VIP Channel">
                     Chille mit all den anderen wirklich Coolen in der VIP Lounge.
-                </Category>
-                <Category id="emoi" name="Dein eigener Emoji">
+                </Feature>
+                <Feature id="emoji" name="Dein eigener Emoji">
                     Füge ab einer Spende von 5€ deinen eigenen Emoji zu unserem Discord hinzu.
-                </Category>
-                <Category id="gfx" name="designhub GFX-Pack">
+                </Feature>
+                <Feature id="gfx" name="designhub GFX-Pack">
                     Wir gewähren dir exklusiven Zugriff auf das private designhub-Pack.
-                </Category>
-                <Category id="gommemode" name="Trololololol!">
+                </Feature>
+                <Feature id="gommemode" name="Trololololol!">
                     Nutze so oft /gommemode wie du willst ohne Coins zu verlieren :gomme:
-                </Category>
-                <Category id="name" name="Du, auf unserer Website">
+                </Feature>
+                <Feature id="name" name="Du, auf unserer Website">
                     Präsentiere dich mit einer Verlinkung zu deinem Social-Media auf unserer Website.
-                </Category>
-                <Category id="donatorrole" name="Donator Rolle">
+                </Feature>
+                <Feature id="donatorrole" name="Donator Rolle">
                     Zeige mit dem @donator-Rang, dass du designhub unterstützt.
-                </Category>
+                </Feature>
             </div>
         </section>
     );
 }
 
-export function Category(props) {
+export function Feature(props) {
     return (
         <div className="item">
             <div className={`icon ${props.id}`} />

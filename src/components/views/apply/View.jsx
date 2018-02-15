78ecc42d665d5f420d8b99d7c820ea850c2b966e
Lukas <lukvonstrom@web.de>
2018-02-15 23:36:12 +0100

    Remove Stub Code

--- src/components/views/apply/View.jsx
+++ src/components/views/apply/View.jsx
@@ -1,57 +1,8 @@
 import React from 'react';
-import { MainTitle } from '../../general/misc/Titles';
+import {MainTitle} from '../../general/misc/Titles';
 import Job from './sections/Job';
 
 export default function Apply() {
-    // NOT IMPLEMENTED YET
-    /*
-    const data = new Map();
-    data.set('name', {
-        type: 'small',
-        label: 'Dein Vor- & Nachname',
-        placeholder: "Lukas's Teddy"
-    });
-    data.set('age', {
-        type: 'number',
-        label: 'Dein Alter',
-        placeholder: '3'
-    });
-    data.set('mail', {
-        type: 'small',
-        label: 'E-Mail',
-        placeholder: 'teddy@lukaas.de'
-    });
-    data.set('discord', {
-        type: 'small',
-        label: 'Discord',
-        placeholder: 'teddy#0815'
-    });
-    data.set('twitter', {
-        type: 'small',
-        label: 'Twitter',
-        placeholder: '@teddygommemode'
-    });
-    data.set('about-me', {
-        type: 'big',
-        label: 'Über mich',
-        placeholder: ''
-    });
-    data.set('motivation', {
-        type: 'big',
-        label: 'Motivation',
-        placeholder: ''
-    });
-    data.set('experience', {
-        type: 'big',
-        label: 'Erfahrungen',
-        placeholder: ''
-    });
-    data.set('credentials', {
-        type: 'big',
-        label: 'Referenzen',
-        placeholder: ''
-    });
-    */
     return (
         <div>
             <section id="apply">
@@ -63,7 +14,7 @@ export default function Apply() {
                 </div>
             </section>
 
-            <Job active={true} rank="Dev" skills="React, NodeJS und Java">
+            <Job active={true} rank="Dev" skills="React und NodeJS ">
                 Du beherschst JavaScript wie deine Muttersprache, träumst in Klassen und auch MySQL jagt dir keine Angst ein? Dann liebst du auch TypeScript und vertraust auf Travis CI?
                 Du willst mithelfen, eine unvergleichliche Community aufzubauen? - <b >Dann bewirb' dich doch als Developer.</b> Wir suchen begeisterte Entwickler, die motiviert sind, sich weiter zu entwickeln und ihre kreative Ideen
                  in unsere Projekte einfließen zu lassen. Für die Arbeit an unserer Website, Discord-Bot und API setzten wir eine Arbeit mit NodeJS-Frameworken wie "express" oder "sequelize" sowie

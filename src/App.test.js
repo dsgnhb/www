2bc56acbd01e0f34f897144d1f1b59beab6e9cfb
LukvonStrom <lukas@fruntke.tech>
2018-03-08 13:53:05 +0100

    :green_heart: Inject localStorage Mock

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import React from 'react';
 
+var localStorageMock = (function() {
+    var store = {};
+    return {
+        getItem: function(key) {
+            return store[key];
+        },
+        setItem: function(key, value) {
+            store[key] = value.toString();
+        },
+        clear: function() {
+            store = {};
+        },
+        removeItem: function(key) {
+            delete store[key];
+        }
+    };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
 

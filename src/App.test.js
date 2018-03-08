509f61381b463951d0fef862999b9121387ada3b
LukvonStrom <lukas@fruntke.tech>
2018-03-08 16:46:27 +0100

    :green_heart: :recycle: Refactor localStorage Mock

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,9 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import React from 'react';
+import localStorageMock from "./localStorageMock";
 
-var localStorageMock = (function() {
-    var store = {};
-    return {
-        getItem: function(key) {
-            return store[key];
-        },
-        setItem: function(key, value) {
-            store[key] = value.toString();
-        },
-        clear: function() {
-            store = {};
-        },
-        removeItem: function(key) {
-            delete store[key];
-        }
-    };
-})();
+
 Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
 it('renders without crashing', () => {
---
+++ src/localStorageMock.js
@@ -0,0 +1,19 @@
+export default class localStorageMock {
+     static store = {};
+
+     static getItem(key) {
+        return this.store[key];
+    }
+
+     static setItem(key, value) {
+        this.store[key] = value.toString();
+    }
+
+     static clear() {
+        this.store = {};
+    }
+
+     static removeItem(key) {
+        delete this.store[key];
+    }
+}

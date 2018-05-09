5572b7e28299c9e1fb732cfd835905ef56b264e2
Flo <x.client.ich@gmail.com>
2018-05-09 21:03:07 +0200

    :bug: idk wth that is

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,8 @@ export default class App extends Component {
             siteId: 1
         });
         this.nm_switcher = this.nm_switcher.bind(this);
-<<<<<<< HEAD
 
         Raven.config('https://1701a9fb09db491fb845dc0e2a763e69@sentry.io/1078222').install(); // eslint-disable-line no-undef
-=======
->>>>>>> 4d40d78c8ea8fa3518781466f0811685c2ec5798
     }
 
     componentDidMount() {
